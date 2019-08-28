18b3a1a811e4509e202f38583f0e04b69a2f7702
diegoangelsoares <diegorangeljpa@gmail.com>
2019-08-27 21:16:24 -0300

    Angular

--- src/main/java/com/diego/AnalisePropostaCartao/AnalisePropostaCartaoApplication.java
+++ src/main/java/com/diego/AnalisePropostaCartao/AnalisePropostaCartaoApplication.java
@@ -2,10 +2,24 @@ package com.diego.AnalisePropostaCartao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.diego.AnalisePropostaCartao.controller.TokenFilter;
+
 
 @SpringBootApplication
 public class AnalisePropostaCartaoApplication {
 
+	@Bean
+	public FilterRegistrationBean filtroJwt() {
+		FilterRegistrationBean frb = new FilterRegistrationBean();
+		frb.setFilter(new TokenFilter());
+		frb.addUrlPatterns("/admin/*");
+		return frb;
+		
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AnalisePropostaCartaoApplication.class, args);
 	}
